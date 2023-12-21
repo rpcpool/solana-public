aae8c2245f55f9c755b9ff4172f75942990b270e
godmodegalactus <godmodegalactus@gmail.com>
2023-12-21 15:43:00 +0100

    Adding more errors for notification

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3489,6 +3489,9 @@ impl Bank {
         let mut status_cache = self.status_cache.write().unwrap();
         assert_eq!(sanitized_txs.len(), execution_results.len());
         for (tx, execution_result) in sanitized_txs.iter().zip(execution_results) {
+            if let TransactionExecutionResult::NotExecuted(err) = execution_result {
+                self.notify_transaction_error(tx, Some(err.clone()));
+            }
             if let Some(details) = execution_result.details() {
                 // Add the message hash to the status cache to ensure that this message
                 // won't be processed again with a different signature.
@@ -3829,6 +3832,7 @@ impl Bank {
             (Ok(()), Some(NoncePartial::new(address, account)))
         } else {
             error_counters.blockhash_not_found += 1;
+            self.notify_transaction_error(tx, Some(TransactionError::BlockhashNotFound));
             (Err(TransactionError::BlockhashNotFound), None)
         }
     }
@@ -3860,6 +3864,10 @@ impl Bank {
                     && self.is_transaction_already_processed(sanitized_tx, &rcache)
                 {
                     error_counters.already_processed += 1;
+                    self.notify_transaction_error(
+                        sanitized_tx,
+                        Some(TransactionError::AlreadyProcessed),
+                    );
                     return (Err(TransactionError::AlreadyProcessed), None);
                 }
 
@@ -4211,6 +4219,7 @@ impl Bank {
                         error_counters.instruction_error += 1;
                     }
                 }
+                self.notify_transaction_error(tx, Some(err.clone()));
                 err
             });
 
@@ -4247,6 +4256,7 @@ impl Bank {
             {
                 status = Err(TransactionError::UnbalancedTransaction);
             } else {
+                self.notify_transaction_error(tx, Some(TransactionError::UnbalancedTransaction));
                 return TransactionExecutionResult::NotExecuted(
                     TransactionError::InstructionError(0, InstructionError::UnbalancedInstruction),
                 );
@@ -4363,6 +4373,23 @@ impl Bank {
         }
 
         loaded_programs_for_txs
+    }
+
+    pub fn notify_transaction_error(
+        &self,
+        transaction: &SanitizedTransaction,
+        result: Option<TransactionError>,
+    ) {
+        if let Some(transaction_result_notifier_lock) = &self.banking_transaction_result_notifier {
+            let transaction_error_notifier = transaction_result_notifier_lock.lock.read();
+            if let Ok(transaction_error_notifier) = transaction_error_notifier {
+                transaction_error_notifier.notify_banking_transaction_result(
+                    transaction,
+                    result,
+                    self.slot,
+                );
+            }
+        }
     }
 
     #[allow(clippy::type_complexity)]
