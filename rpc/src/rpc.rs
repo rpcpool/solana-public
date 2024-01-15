c0fb87589be61cae6219d034e8f55337923c8b65
linuskendall <git@linuskendall.com>
2024-01-15 08:33:41 +0000

    rpc: add solend patch (gPA filter)
    Adds a patch that allows more complex queries for gPA.

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -2614,6 +2614,7 @@ fn get_spl_token_owner_filter(program_id: &Pubkey, filters: &[RpcFilterType]) ->
                 }
             }
             RpcFilterType::TokenAccountState => token_account_state_filter = true,
+            RpcFilterType::ValueCmp(_) => {}
         }
     }
     if data_size_filter == Some(account_packed_len as u64)
@@ -2664,6 +2665,7 @@ fn get_spl_token_mint_filter(program_id: &Pubkey, filters: &[RpcFilterType]) -> 
                 }
             }
             RpcFilterType::TokenAccountState => token_account_state_filter = true,
+            RpcFilterType::ValueCmp(_) => {}
         }
     }
     if data_size_filter == Some(account_packed_len as u64)
