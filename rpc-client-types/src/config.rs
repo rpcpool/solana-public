04affca5366acc5fe1a92232819bf1c845d961be
Kirill Fomichev <fanatid@ya.ru>
2024-02-18 21:09:37 -0500

    rpc: add config option to getRPF (#217)

--- rpc-client-types/src/config.rs
+++ rpc-client-types/src/config.rs
@@ -352,3 +352,9 @@ pub struct RpcContextConfig {
     pub commitment: Option<CommitmentConfig>,
     pub min_context_slot: Option<Slot>,
 }
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcRecentPrioritizationFeesConfig {
+    pub percentile: Option<u16>,
+}
