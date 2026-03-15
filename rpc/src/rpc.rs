f7be428d6b6e26b2315e5f806a1ab64392ade5ce
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2026-03-15 13:21:17 +0000

    rpc: add method sanitize_transaction (#176)
    Signed-off-by: GitHub <noreply@github.com>

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -3534,6 +3534,14 @@ pub mod rpc_full {
             config: Option<RpcSendTransactionConfig>,
         ) -> Result<String>;
 
+        #[rpc(meta, name = "sanitizeTransaction")]
+        fn sanitize_transaction(
+            &self,
+            meta: Self::Metadata,
+            data: String,
+            config: Option<RcpSanitizeTransactionConfig>,
+        ) -> Result<()>;
+
         #[rpc(meta, name = "simulateTransaction")]
         fn simulate_transaction(
             &self,
@@ -3975,6 +3983,49 @@ pub mod rpc_full {
                 durable_nonce_info,
                 max_retries,
             )
+        }
+
+        fn sanitize_transaction(
+            &self,
+            meta: Self::Metadata,
+            data: String,
+            config: Option<RcpSanitizeTransactionConfig>,
+        ) -> Result<()> {
+            let RcpSanitizeTransactionConfig {
+                sig_verify,
+                commitment,
+                encoding,
+                min_context_slot,
+            } = config.unwrap_or_default();
+            let tx_encoding = encoding.unwrap_or(UiTransactionEncoding::Base58);
+            let binary_encoding = tx_encoding.into_binary_encoding().ok_or_else(|| {
+                Error::invalid_params(format!(
+                    "unsupported encoding: {tx_encoding}. Supported encodings: base58, base64"
+                ))
+            })?;
+            let (_wire_transaction, unsanitized_tx) =
+                decode_and_deserialize::<VersionedTransaction>(data, binary_encoding)?;
+
+            let bank = &*meta.get_bank_with_config(RpcContextConfig {
+                commitment,
+                min_context_slot,
+            })?;
+            let transaction = sanitize_transaction(
+                unsanitized_tx,
+                bank,
+                bank.get_reserved_account_keys(),
+                bank.feature_set
+                    .is_active(&agave_feature_set::static_instruction_limit::id()),
+                bank.feature_set
+                    .is_active(&agave_feature_set::limit_instruction_accounts::id()),
+            )?;
+            if sig_verify {
+                transaction.verify().map_err(|e| {
+                    Error::from(RpcCustomError::TransactionPrecompileVerificationFailure(e))
+                })?;
+            }
+
+            Ok(())
         }
 
         fn simulate_transaction(
