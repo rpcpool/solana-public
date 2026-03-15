f3fe672e78238f8634a0bc6df03788e466111cbd
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2026-03-15 15:30:08 +0000

    rpc: add solend patch (gPA filter)
    Adds a patch that allows more complex queries for gPA.
    
    Signed-off-by: GitHub <noreply@github.com>

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -2642,6 +2642,7 @@ fn get_spl_token_owner_filter(
                 }
             }
             RpcFilterType::TokenAccountState => token_account_state_filter = true,
+            RpcFilterType::ValueCmp(_) => {}
         }
     }
     if data_size_filter == Some(account_packed_len as u64)
@@ -2693,6 +2694,7 @@ fn get_spl_token_mint_filter(
                 }
             }
             RpcFilterType::TokenAccountState => token_account_state_filter = true,
+            RpcFilterType::ValueCmp(_) => {}
         }
     }
     if data_size_filter == Some(account_packed_len as u64)
