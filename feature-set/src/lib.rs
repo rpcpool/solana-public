d77a676aa9c9f42133fca38c311cd6f0ddc67744
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2026-03-15 13:21:17 +0000

    rpc: add method sanitize_transaction (#176)
    Signed-off-by: GitHub <noreply@github.com>

--- feature-set/src/lib.rs
+++ feature-set/src/lib.rs
@@ -1136,7 +1136,9 @@ pub mod alpenglow {
     // Used to activate alpenglow in local-cluster tests without exposing the actual feature's private key
     #[cfg(feature = "dev-context-only-utils")]
     pub static TEST_KEYPAIR: LazyLock<Keypair> = LazyLock::new(|| {
-        let keypair = Keypair::from_base58_string("2Vzd6oTWU4RtM5UmsSyBH3tAhPSi1sKqMeMC8bF1jzHHLBMRhEWtrfmBV4EmwQbGSwkunk5Wy67kXNAL1ZL1xQhR");
+        let keypair = Keypair::from_base58_string(
+            "2Vzd6oTWU4RtM5UmsSyBH3tAhPSi1sKqMeMC8bF1jzHHLBMRhEWtrfmBV4EmwQbGSwkunk5Wy67kXNAL1ZL1xQhR",
+        );
         assert_eq!(keypair.pubkey(), super::alpenglow::id());
         keypair
     });
