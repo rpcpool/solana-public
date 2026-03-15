a3d21b6e3b73ff7e322e8b2cb37b645efb664b0b
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2026-03-15 16:52:04 +0000

    feat(geyser): load ALT for V0 messages on deshred-transactions (#312)
    * feat(geyser): load ALT for V0 messages on deshred-transactions
    
    * chore(geyser): add metrics for alt loading and deshred-txn
    
    Signed-off-by: GitHub <noreply@github.com>

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -1336,6 +1336,7 @@ impl Validator {
                         deshred_transaction_notifier.clone(),
                         exit.clone(),
                         max_slots.clone(),
+                        bank_forks.clone(),
                     );
                     (
                         Some(completed_data_sets_sender),
